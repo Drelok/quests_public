939302e2f17721bace18f387671e6c8b08bbffde
Kinglykrab <kinglykrab@gmail.com>
2025-02-19 23:44:21 -0500

    Update Grand_Librarian_Maelin.lua

--- poknowledge/Grand_Librarian_Maelin.lua
+++ poknowledge/Grand_Librarian_Maelin.lua
@@ -76,8 +76,8 @@ function event_say(e)
 			else
 				e.self:Say("You lack the necessary requirements for me to speak with you.")
 				e.other:Message(MT.Yellow, "Your missing flags are as follows:")
-				for flag, _ in pairs(missing_flags) do
-					local flag_name = string.gsub(flag, "pop.flags.", "")
+				for missing_flag, _ in pairs(missing_flags) do
+					local flag_name = string.gsub(missing_flag, "pop.flags.", "")
 					e.other:Message(MT.Yellow, flag_name)
 				end
 			end
