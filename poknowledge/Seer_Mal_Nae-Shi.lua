ec03abd1097a20c26c28e9fdc04bf6532012fadd
Kinglykrab <kinglykrab@gmail.com>
2025-02-20 15:32:13 -0500

    Update Seer_Mal_Nae-Shi.lua

--- poknowledge/Seer_Mal_Nae-Shi.lua
+++ poknowledge/Seer_Mal_Nae-Shi.lua
@@ -53,7 +53,7 @@ function event_say(e)
 			local match_string = (flag:findi("pop.flags.")) and "pop.flags." or "pop.alt."
 			local flag_name = string.gsub(flag, match_string, "")
 
-			if type == "Alternate Access" then
+			if type == "Alternate Access" and flag_name ~= "hedge" then
 				flag_name = eq.get_zone_long_name_by_name(flag_name)
 			end
 
