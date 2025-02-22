3831d76e2f7a9ba04669df405383e4767ed1f2e6
mute <natanx@gmail.com>
2025-02-22 15:29:17 -0800

    Fix poxbourne flagging

--- potranquility/Elder_Poxbourne.lua
+++ potranquility/Elder_Poxbourne.lua
@@ -1,7 +1,7 @@
 function event_say(e)
-	local poxbourne_bucket = tonumber(e.other:GetAccountBucket("pop.flags.poxbourne")) or 0
+	local hedge_bucket = tonumber(e.other:GetAccountBucket("pop.flags.hedge")) or 0
 	local terris_bucket = tonumber(e.other:GetAccountBucket("pop.flags.terris")) or 0
-	if poxbourner_bucket == 1 and terris_bucket == 1 then
+	if hedge_bucket == 1 and terris_bucket == 1 then
 		if e.message:findi("Hail") then
 			e.self:Say("Finally, after all this time, my mind is at peace... thank you, $name, Subduer of Torment.")
 			e.other:SetAccountBucket("pop.flags.poxbourne", "1")
