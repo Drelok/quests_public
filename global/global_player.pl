b3631f4671c06e56f5c623e138a82ab8a7a65d1a
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2025-02-23 12:12:04 -0500

    Update global_player.pl

--- global/global_player.pl
+++ global/global_player.pl
@@ -379,76 +379,77 @@ sub EVENT_SAY {
             if ($tar_client && $tar_client->IsClient()) {
                 $tar_client = $tar_client->CastToClient();
             } else {
-				return;
-			}
-
-			# Validate that there is exactly one argument which is a number
-            if ($arguments =~ /^\s*(\d+)\s*$/) {
-				my $number = $1; # Captures the number
-				# Proceed with awarding the title using $number
-                    
-				$client->Message(13, "Awarding TitleSet $number to " . $tar_client->GetName());
-				plugin::AddTitleFlag($number, $tar_client->CastToClient());
-				plugin::CommonCharacterUpdate($tar_client->CastToClient());
-				$tar_client->Signal(1);
-			} else {
-				$client->Message(13, "Invalid input. Please provide a single numeric argument.");
-			}
-        } elsif ($text=~/#setpopflag\s+(\S+)(?:\s+(\d))?/i) {
-            my ($flag, $number) = ($1, $2 // 1);  # Default $number to 1 if not provided
-
-            my $tar_client = $client->GetTarget();
-            if ($tar_client && $tar_client->IsClient()) {
-                $tar_client = $tar_client->CastToClient();
-            } else {
                 return;
             }
 
-            if ($number >= 0 && $number <= 9) {
-                my $client_name = $tar_client->GetCleanName();
-                $tar_client->SetAccountBucket("pop.flags.$flag", "$number");
-                $tar_client->Message(4, "You receive a character flag!");
-                $client->Message(4, "'$flag' flag set to '$number' for $client_name.");
+            # Validate that there is exactly one argument which is a number
+            if ($arguments =~ /^\s*(\d+)\s*$/) {
+                my $number = $1; # Captures the number
+                # Proceed with awarding the title using $number
+                    
+                $client->Message(13, "Awarding TitleSet $number to " . $tar_client->GetName());
+                plugin::AddTitleFlag($number, $tar_client->CastToClient());
+                plugin::CommonCharacterUpdate($tar_client->CastToClient());
+                $tar_client->Signal(1);
             } else {
-                $client->Message(13, "Invalid number. Please provide a number between 0 and 9.");
+                $client->Message(13, "Invalid input. Please provide a single numeric argument.");
             }
-		} elsif ($text=~/#resetpopflags/i) {
-       		my $tar_client = $client->GetTarget();
-			if ($tar_client && $tar_client->IsClient()) {
-              	$tar_client = $tar_client->CastToClient();
-  			} else {
-				return;
-			}
-
-            my @zoneflags = POPZoneFlags();
-            foreach my $zoneflag (@zoneflags) {
-                $client->ClearZoneFlag($zoneflag);
-            }
-  
-			my $client_name = $tar_client->GetCleanName();
-       		$tar_client->DeleteAccountBucket("pop");
-		 	$tar_client->Message(4, "Your Planes of Power flags have been reset.");
-			$client->Message(4, "Planes of Power flags reset for $client_name.");
-   		}
+        }
     }
 
-	if ($text=~/#pop/i) {
-		my @flags = POPFlags();
+    if ($text=~/#setpopflag\s+(\S+)(?:\s+(\d))?/i) {
+        my ($flag, $number) = ($1, $2 // 1);  # Default $number to 1 if not provided
 
-		quest::message(315, "Your Planes of Power flags are as follows:");
+        my $tar_client = $client->GetTarget();
+        if ($tar_client && $tar_client->IsClient()) {
+            $tar_client = $tar_client->CastToClient();
+        } else {
+            return;
+        }
 
-		foreach my $flag (sort {$a cmp $b} @flags) {
-			my $current_value = $client->GetAccountBucket($flag);
-			if ($current_value eq "") {
-				$current_value = 0;
-			}
+        if ($number >= 0 && $number <= 9) {
+            my $client_name = $tar_client->GetCleanName();
+            $tar_client->SetAccountBucket("pop.flags.$flag", "$number");
+            $tar_client->Message(4, "You receive a character flag!");
+            $client->Message(4, "'$flag' flag set to '$number' for $client_name.");
+        } else {
+            $client->Message(13, "Invalid number. Please provide a number between 0 and 9.");
+        }
+    } elsif ($text=~/#resetpopflags/i) {
+        my $tar_client = $client->GetTarget();
+        if ($tar_client && $tar_client->IsClient()) {
+            $tar_client = $tar_client->CastToClient();
+        } else {
+            return;
+        }
 
-			$flag =~ s/pop\.flags\.//ig;
+        my @zoneflags = POPZoneFlags();
+        foreach my $zoneflag (@zoneflags) {
+            $tar_client->ClearZoneFlag($zoneflag);
+        }
+  
+        my $client_name = $tar_client->GetCleanName();
+        $tar_client->DeleteAccountBucket("pop");
+        $tar_client->Message(4, "Your Planes of Power flags have been reset.");
+        $client->Message(4, "Planes of Power flags reset for $client_name.");
+    } elsif ($text=~/#pop/i) {
+        my @flags = POPFlags();
 
-			quest::message(315, "Flag: $flag Current: $current_value");
-		}
-	}
+        quest::message(315, "Your Planes of Power flags are as follows:");
+
+        foreach my $flag (sort {$a cmp $b} @flags) {
+            my $current_value = $client->GetAccountBucket($flag);
+            if ($current_value eq "") {
+                $current_value = 0;
+            }
+
+            $flag =~ s/pop\.flags\.//ig;
+
+            quest::message(315, "Flag: $flag Current: $current_value");
+        }
+    }
 }
+
 
 sub POPFlags {
 	my @flags = (
