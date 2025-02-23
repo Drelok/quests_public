88e357db00dbd549057ad59c507b604a23da3703
Kinglykrab <kinglykrab@gmail.com>
2025-02-23 00:03:13 -0500

    Merge branch 'staging' of https://github.com/The-Heroes-Journey-EQEMU/quests_public into staging

--- potranquility/Elder_Poxbourne.lua
+++ potranquility/Elder_Poxbourne.lua
@@ -3,7 +3,12 @@ function event_say(e)
 	local terris_bucket = tonumber(e.other:GetAccountBucket("pop.flags.terris")) or 0
 	if hedge_bucket == 1 and terris_bucket == 1 then
 		if e.message:findi("Hail") then
-			e.self:Say("Finally, after all this time, my mind is at peace... thank you, $name, Subduer of Torment.")
+			e.self:Say(
+				string.format(
+					"Finally, after all this time, my mind is at peace... thank you, %s, Subduer of Torment.",
+					e.other:GetCleanName()
+				)
+			)
 			e.other:SetAccountBucket("pop.flags.poxbourne", "1")
 			e.other:Message(MT.LightBlue, "You receive a character flag!")
 		end
