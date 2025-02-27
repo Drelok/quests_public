5379686234c94a79e701a3c05b2174096f5a4cf7
mute <natanx@gmail.com>
2025-02-27 08:56:53 -0800

    number

--- potranquility/Sarhya_the_Dawnbreeze.lua
+++ potranquility/Sarhya_the_Dawnbreeze.lua
@@ -1,13 +1,13 @@
 function event_say(e)
     local pop_tactics = tonumber(e.other:GetAccountBucket("pop.flags.rallos")) or 0
     
-    if e.message:findi("hail") and pop_tactics == "1" then
+    if e.message:findi("hail") and pop_tactics == 1 then
         e.self:Say("It appears that you are ready to travel into the Elemental Planes. Beware many of the gods there have set up wards to protect them against assaults. Most I cannot speak about; however I do have information on the Circle of Protection that guards Xegony, in her [" .. eq.say_link("Plane of Air") .. "].")
     elseif e.message:findi("hail") then
         e.self:Say("Well met I am keeper of the Portal to the [" .. eq.say_link("Plane of Air") .. "].")
     end
 
-    if e.message:findi("plane of air") and pop_tactics == "1" then
+    if e.message:findi("plane of air") and pop_tactics == 1 then
         e.self:Say("Xegony has created four avatars, each one embodies a different aspect of her power, and each of these avatars was created with a portion of her very essence. The story goes, the one who possesses each of these essences may create a key to break Xegony's ward and enter into her inner sanctum. I don't know the truth behind this, as I have neither the power, nor the desire to challenge the Avatars myself. I have fashioned this, which I believe will form the four essences into a key.")
         e.other:SummonItem(17888)  -- Pouch of Swirling Winds
     elseif e.message:findi("plane of air") then
