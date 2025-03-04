636fc91457ae26d092183844f0134cad1c81e604
mute <natanx@gmail.com>
2025-03-04 15:40:30 -0800

    perl

--- ponightmare/#Aid_Eino.pl
+++ ponightmare/#Aid_Eino.pl
@@ -5,7 +5,7 @@ sub EVENT_SPAWN {
 
 sub EVENT_PROXIMITY_SAY {
   if (($zonehour > 19 || $zonehour < 6) && ($text=~/quellious be my guide/i) && ($client->GetAccountBucket("eino-trigger") == "1")) {
-    client->SetAccountBucket("eino-trigger", "0");
+    $client->SetAccountBucket("eino-trigger", "0");
     quest::unique_spawn(204071,0,0,1685,-515,215,310); # NPC: Aid_Eino
     quest::depop_withtimer();
   }
