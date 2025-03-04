75df72c2d34a71d899dce01fbc742070aefe34ca
mute <natanx@gmail.com>
2025-03-04 15:35:29 -0800

    Rework Aid Eno to use Account Buckets

--- ponightmare/#Aid_Eino.pl
+++ ponightmare/#Aid_Eino.pl
@@ -4,7 +4,8 @@ sub EVENT_SPAWN {
 }
 
 sub EVENT_PROXIMITY_SAY {
-  if (($zonehour > 19 || $zonehour < 6) && ($text=~/quellious be my guide/i) && defined($qglobals{"EinoTrigger"}) && ($qglobals{"EinoTrigger"} == 1)) {
+  if (($zonehour > 19 || $zonehour < 6) && ($text=~/quellious be my guide/i) && ($client->GetAccountBucket("eino-trigger") == "1")) {
+    client->SetAccountBucket("eino-trigger", "0");
     quest::unique_spawn(204071,0,0,1685,-515,215,310); # NPC: Aid_Eino
     quest::depop_withtimer();
   }
