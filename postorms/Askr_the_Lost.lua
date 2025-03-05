4b5b460370fe9c012cbb9a099813c39407a157e2
mute <natanx@gmail.com>
2025-03-04 20:23:15 -0800

    Simplifies Askr's verbose dialogue

--- postorms/Askr_the_Lost.lua
+++ postorms/Askr_the_Lost.lua
@@ -5,22 +5,22 @@ function event_say(e)
 			local problem_link = eq.silent_say_link("what problem", "problem")
 			e.self:Emote(
 				string.format(
-					"looks up at you, and when he does, you take note of the scraggly beard on his weather-worn face; the scent of ale permeates your senses and pushes you to look away slightly. In an apparent drunken stupor, Askr begins to speak with surprising clarity. 'Here to wash the windows and clean the flooring, are you? No? Well, you can't be much help then. And the likes of you obviously wouldn't be able to get rid of the massive [%s] we're having. Bah, says I. You can help me no more than any of these other vagrants. Leave me alone to my ale and my misery.'",
+					"gazes at you with a rough beard and reeking of ale. 'Not here to clean? You wouldn't handle this [%s] anyway. Leave me to my drink.'",
 					problem_link
 				)
 			)
 		elseif e.message:findi("what problem") then
-			e.self:Say("Askr points drunkenly towards the exit of the cave. 'Have you not seen the foul denizens of destruction outside? Hrmph! Going everywhere they please, pillaging, plundering. . . I'm lucky to have survived this long. Bah, it doesn't matter, there's nothing that anyone can do to stop them, and that is why I'm still stuck here with my *hic* potions.'")
+			e.self:Say("Askr gestures toward the cave exit. 'Can't you see the monsters outside, pillaging at will? I'm doomed with my *hic* potions.'")
 		elseif e.message:findi("yes") then
 			local askr_bucket = tonumber(e.other:GetAccountBucket("pop.flags.askr")) or 0
 			local continue_link = eq.silent_say_link("continue")
 			if askr_bucket == 1 then
 				e.self:Say(
 					string.format(
-					"Truly, I am amazed that someone of your caliber was able to do what I could not. I have been stuck in these desolate caves for so long, I had almost lost hope that I would ever find a way to get rid of those accursed giants. But you...  you have been able to best at least one of them. It is reasonable to suggest that if you are able to dispatch one, you may be able to dispatch others. I need to know for sure if you are as capable an individual as it appears you are. This place was once beautiful and serene, with the great Karana's showers falling day and night soothing the lands and the creatures that inhabit it. Alas, there was an invasion of a superior force -- the storm giants which you have encountered. They laid waste to the lands, burned down trees, and have caused so much havoc I don't even know what is to become of it. Originally, they came as one group and crossed the breadth of the land pillaging and plundering all that they could find. Over time, though, they grew apart and distinct factions grew around Mount Grenidor.[%s]",
-					continue_link
+						"Remarkable—you've slain a giant. I've been trapped here by storm giants for too long. They once moved as one but now split around Mount Grenidor. [%s]",
+						continue_link
+					)
 				)
-			)
 			end
 		elseif e.message:findi("continue") then
 			local askr_bucket = tonumber(e.other:GetAccountBucket("pop.flags.askr")) or 0
@@ -28,13 +28,13 @@ function event_say(e)
 				local continue_link = eq.silent_say_link("continue")
 				e.self:Say(
 					string.format(
-						"To the south of Grenidor lay Srerendi, Shores of the Lost and the Srerendi storm giants. Born of the ocean and the cool air, they lay claim to the shores and the plains inland as their own. To the west of Grenidor lay Krendic, Sands of Chaos and the Krendic storm giants. They have skin that is thick as dirt and blend in well with their surroundings. To the north of the great mount lay Kelek`Vor, the Forest of Tears and the Kelek`Vor storm giants. They are born of the wood and take great pleasure in seeing it used to their own purposes.' [%s]",
+						"South of Grenidor: Srerendi giants rule the shores. West: Krendic giants, tough and gritty. North: Kelek`Vor giants, masters of the woods. [%s]",
 						continue_link
 					)
 				)
 				e.other:SetAccountBucket("pop.flags.askr", "2")
 			elseif askr_bucket == 2 then
-				e.self:Say("I must know if you are truly capable of defeating the storm giants that abound here. To do this, you must slay one of each of the three factions and return to me something specific to each tribe: from the Srerendi storm giants, return their shorn beard; from the Krendic storm giants, a piece of their rocky carcass; from the Kelek`Vor storm giants, the crest from their warrior garments. These three items will ensure that you indeed do have the ability to defeat the foul denizens that have seized control of these lands. When you have acquired the three of these items, seal them in this bag and return them to me, so that I know that you are indeed capable of fulfilling a hero's destiny.")
+				e.self:Say("Prove your strength: slay one giant from each faction. Return a Srerendi beard, a Krendic stone, and a Kelek`Vor crest in this bag.")
 				e.other:SummonItem(17192) -- Askr's Bag of Verity
 			end
 		elseif e.message:findi("It was me") then
@@ -43,7 +43,7 @@ function event_say(e)
 				local paying_attention_link = eq.silent_say_link("I am paying attention", "paying attention")
 				e.self:Say(
 					string.format(
-						"Truly, I am amazed that someone of your caliber was able to do what I could not. I have been stuck in these desolate caves for so long, I had almost lost hope that I would ever find a way to get rid of those accursed giants. But you. . . you have been able to best at least one of them. It is reasonable to suggest that if you are able to dispatch one, you may be able to dispatch others. I need to know for sure if you are as capable an individual as it appears you are. This is very important, so are you [%s]?",
+						"Impressive—you've slain a giant. Prove you're as skilled as you seem. Are you [%s]?",
 						paying_attention_link
 					)
 				)
@@ -54,7 +54,7 @@ function event_say(e)
 				local continue_link = eq.silent_say_link("continue")
 				e.self:Say(
 					string.format(
-						"This place was once beautiful and serene, with the great Karana's showers falling day and night soothing the lands and the creatures that inhabit it. Alas, there was an invasion of a superior force -- the storm giants which you have encountered. They laid waste to the lands, burned down trees, and have caused so much havoc I don't even know what is to become of it. Originally, they came as one group and crossed the breadth of the land pillaging and plundering all that they could find. Over time, though, they grew apart and distinct factions grew around Mount Grenidor.' [%s]",
+						"Once, this land was serene under Karana's care. Then the storm giants invaded and split into factions around Mount Grenidor. [%s]",
 						continue_link
 					)
 				)
@@ -62,7 +62,7 @@ function event_say(e)
 		elseif e.message:findi("Bastion of Thunder") then
 			local askr_bucket = tonumber(e.other:GetAccountBucket("pop.flags.askr")) or 0
 			if askr_bucket == 3 then
-				e.self:Say("To be honest, I cannot say that it is entirely possible, what with the ferocity of the giants and their reluctance to leave the lands they now inhabit. From what I do know, each of the Leaders of each tribe has one piece to allow entrance into the Bastion of Thunder. When two of these pieces are combined, they form an esoteric medallion that allows instant passage to the thunderous plains beyond. If you can find and seal two pieces of the medallion in this bag, I will be able to forge them into the medallion that will help you on your journey.")
+				e.self:Say("The giants are fierce. Each tribe leader holds a piece to open the Bastion of Thunder. Seal two pieces in this bag, and I'll forge your medallion.")
 				e.other:SummonItem(17192) -- Askr's Bag of Verity
 			end
 		end
@@ -83,17 +83,17 @@ function event_trade(e)
 				item_lib.check_turn_in(e.trade, {item1 = 28781}) or
 				item_lib.check_turn_in(e.trade, {item1 = 28782})
 			) then
-				e.self:Say("The drunken stupor that Askr had been in earlier appears to vanish in an instant; a glimmer of hope now flashes in the depths of his eyes. 'This is the head of a giant. . . a storm giant from outside the caves.' Askr ponders what this revelation could mean. He mutters to himself, 'If it is possible that someone has indeed bested a giant here in the planes, then perhaps it is possible. . .' He looks back at you very sternly and asks, 'Tell me now and be truthful, was it you who severed the head of this giant from his body?'")
+				e.self:Say("Soberly, Askr eyes the giant's head. 'Did you really sever this storm giant's head?'")
 				e.other:SetAccountBucket("pop.flags.askr", "1")
 			end
 		elseif askr_bucket == 2 then
 			if item_lib.check_turn_in(e.trade, {item1 = 11487}) then
-				e.self:Emote("looks over the remnants of the storm giants in his hands then looks up at you with a glimmer of hope in his eyes. 'Then it's true! You have the means to push back the scourge of giants. . . you must go forth to the Bastion of Thunder and finish there what you have started here. It is imperative that you clear the forces of giants from the Bastion so that order can be restored to the thunderous reaches beyond.'")
+				e.self:Emote("examines the remains and then says, 'It's true—you can push back the giants. Now head to the Bastion of Thunder and finish what you've started.'")
 				e.other:SetAccountBucket("pop.flags.askr", "3")
 			end
 		elseif askr_bucket == 3 then
 			if item_lib.check_turn_in(e.trade, {item1 = 11488}) then
-				e.self:Emote("The mystical medallion given to you by Askr settles around your neck and then disappears into nothingness. There is no call for alarm, for the medallion is now a part of your soul. Askr the Lost says 'You have retrieved the pieces! You are well on your way to pushing forth on your quest to return balance to the plains of thunder. The most difficult part lies before you. . . deep in the heart of Mount Grenidor, there is a tempest that rages on eternally. It is there that you must present your medallion to the skies above to be transported to the citadel of thunderous might. Take the medallion and go forth, then deliver these storm giants to their creator!'")
+				e.self:Emote("A mystical medallion appears and vanishes. Askr says, 'You've gathered the pieces! Now take the medallion to Mount Grenidor's heart and deliver the giants to their maker.'")
 				e.other:SetAccountBucket("pop.flags.askr", "4")
 				e.other:Message(MT.LightBlue, "You receive a character flag!")
 			end
