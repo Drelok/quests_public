efd9ef9836968597697c96564c834f021d4f38e2
Kinglykrab <kinglykrab@gmail.com>
2025-03-06 00:25:34 -0500

    Update Arlyxir.lua

--- solrotower/Arlyxir.lua
+++ solrotower/Arlyxir.lua
@@ -1,34 +1,42 @@
 function event_combat(e)
-	if (e.joined == true) then
-		eq.set_timer("OOBcheck", 6 * 10000);
-		eq.set_timer("heal", 7500000);
+	if e.joined then
+		eq.set_timer("Out of Bounds", 60 * 1000) -- 60 Seconds
+		eq.set_timer("Heal", 125 * 60 * 1000) -- 2 Hours and 5 Minutes
 	else
-		eq.stop_timer("OOBcheck");
-		eq.stop_timer("heal");
+		eq.stop_timer("Out of Bounds")
+		eq.stop_timer("Heal")
 	end
 end
 
 function event_timer(e)
-	if(e.timer=="OOBcheck") then
-	eq.stop_timer("OOBcheck");
-		if (e.self:GetY() < 1240) then
+	if e.timer == "Out of Bounds" then
+		eq.stop_timer("Out of Bounds")
+		if e.self:GetY() < 1240 then
 			e.self:CastSpell(2830, e.self:GetID())
-			e.self:SetHP(e.self:GetMaxHP());
-			e.self:GotoBind();
-			e.self:WipeHateList();
+			e.self:SetHP(e.self:GetMaxHP())
+			e.self:GotoBind()
+			e.self:WipeHateList()
 		else
-			eq.set_timer("OOBcheck", 6 * 10000);
+			eq.set_timer("Out of Bounds", 6 * 10000)
 		end
-	elseif ( e.timer == "heal" ) then
-		e.self:Emote("is immolated in flames, and is reborn!");
-		e.self:Heal();
-		e.self:CastSpell(1281, e.self:GetTarget():GetID()); -- Searing Flames
+	elseif e.timer == "Heal" then
+		e.self:Emote("is immolated in flames, and is reborn!")
+		e.self:Heal()
+		e.self:CastSpell(1281, e.self:GetTarget():GetID()) -- Searing Flames
 	end
 end
 
 function event_death_complete(e)
-eq.unique_spawn(202367, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()); -- NPC: A_Planar_Projection
-eq.spawn2(212074,0,0,1713,1206,627,264); -- a_warder_of_Arlyxir (212074)
-eq.spawn2(212074,0,0,1738,1206,627,264); -- a_warder_of_Arlyxir (212074)
-eq.spawn2(212074,0,0,1726,1146,612,264); -- a_warder_of_Arlyxir (212074)
+	eq.unique_spawn(202367, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()) -- NPC: A_Planar_Projection
+	eq.spawn2(212074, 0, 0, 1713, 1206, 627, 264) -- a_warder_of_Arlyxir (212074)
+	eq.spawn2(212074, 0, 0, 1738, 1206, 627, 264) -- a_warder_of_Arlyxir (212074)
+	eq.spawn2(212074, 0, 0, 1726, 1146, 612, 264) -- a_warder_of_Arlyxir (212074)
+end
+
+function event_killed_merit(e)
+	local arlyxir_bucket = tonumber(e.other:GetAccountBucket("pop.flags.arlyxir")) or 0
+	if arlyxir_bucket == 0 then
+		e.other:SetAccountBucket("pop.flags.arlyxir", "1")
+		e.other:Message(MT.LightBlue, "You receive a character flag!")
+	end
 end
