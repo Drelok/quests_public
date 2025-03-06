c6e575b3a22866f9bc74f389ea3149dc1270deaf
Kinglykrab <kinglykrab@gmail.com>
2025-03-06 00:37:27 -0500

    Update Rizlona.lua and #Rizlona.lua

--- solrotower/#Rizlona.lua
+++ solrotower/#Rizlona.lua
@@ -1,35 +1,43 @@
-function event_death_complete(e)
-eq.unique_spawn(202368, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()); -- NPC: A_Planar_Projection
-eq.spawn2(212076,0,0,-1118,2024,-908,258); --a_warder_of_Rizlona (212076)
-eq.spawn2(212076,0,0,-1101,1978,-920,258); --a_warder_of_Rizlona (212076)
-eq.spawn2(212076,0,0,-1086,2024,-908,258); --a_warder_of_Rizlona (212076)
-end
-
 function event_combat(e)
-if (e.joined == true) then
-eq.set_timer("OOBcheck", 6 * 1000);
-eq.stop_timer("depop");
-else
-eq.stop_timer("OOBcheck");
-eq.set_timer("depop", 1800 * 1000);
-end
+	if e.joined then
+		eq.set_timer("Out of Bounds", 6 * 1000) -- 6 Seconds
+		eq.stop_timer("Depop")
+	else
+		eq.set_timer("Depop", 30 * 60 * 1000) -- 30 Minutes
+		eq.stop_timer("Out of Bounds")
+	end
 end
 
 
 function event_timer(e)
-	if(e.timer=="OOBcheck") then
-		eq.stop_timer("OOBcheck");
-			if (e.self:GetY() < 2076) then
-				e.self:Say("If you wish to challenge me, you must do it on my terms!");
-				e.self:CastSpell(2830, e.self:GetID())
-				e.self:SetHP(e.self:GetMaxHP());
-				e.self:GMMove(-1104,2384,-905,256);
-				e.self:WipeHateList();
-			else
-				eq.set_timer("OOBcheck", 6 * 1000);
-			end
-	elseif(e.timer == "depop") then
-		eq.stop_timer("depop");
-		eq.depop();
+	if e.timer == "Out of Bounds" then
+		eq.stop_timer("Out of Bounds")
+		if e.self:GetY() < 2076 then
+			e.self:Say("If you wish to challenge me, you must do it on my terms!")
+			e.self:CastSpell(2830, e.self:GetID())
+			e.self:SetHP(e.self:GetMaxHP())
+			e.self:GMMove(-1104, 2384, -905, 256)
+			e.self:WipeHateList()
+		else
+			eq.set_timer("Out of Bounds", 6 * 1000) -- 6 Seconds
+		end
+	elseif e.timer == "Depop" then
+		eq.stop_timer("Depop")
+		eq.depop()
+	end
+end
+
+function event_death_complete(e)
+	eq.unique_spawn(202368, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()) -- NPC: A_Planar_Projection
+	eq.spawn2(212076, 0, 0, -1118, 2024, -908, 258) --a_warder_of_Rizlona (212076)
+	eq.spawn2(212076, 0, 0, -1101, 1978, -920, 258) --a_warder_of_Rizlona (212076)
+	eq.spawn2(212076, 0, 0, -1086, 2024, -908, 258) --a_warder_of_Rizlona (212076)
+end
+	
+function event_killed_merit(e)
+	local rizlona_bucket = tonumber(e.other:GetAccountBucket("pop.flags.rizlona")) or 0
+	if rizlona_bucket == 0 then
+		e.other:SetAccountBucket("pop.flags.rizlona", "1")
+		e.other:Message(MT.LightBlue, "You receive a character flag!")
 	end
 end
