539bc3d6d1c01a4ba07dd732c8b806bcfc96d211
Kinglykrab <kinglykrab@gmail.com>
2025-03-06 01:04:25 -0500

    Update 218068.lua

--- poeartha/218068.lua
+++ poeartha/218068.lua
@@ -11,8 +11,11 @@ end
 
 function event_say(e)
 	if e.message:findi("Hail") then
-		e.other:SetZoneFlag(222)
-		e.other:SetAccountBucket("pop.flags.arbitor", "1")
-		e.other:Message(MT.LightBlue, "You receive a character flag!")
+		local arbitor_bucket = tonumber(e.other:GetAccountBucket("pop.flags.arbitor")) or 0
+		if arbitor_bucket == 0 then
+			e.other:SetZoneFlag(222)
+			e.other:SetAccountBucket("pop.flags.arbitor", "1")
+			e.other:Message(MT.LightBlue, "You receive a character flag!")
+		end
 	end
 end
