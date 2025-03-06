93ff70c78ecbb31767ebaade395b474a09106b61
Kinglykrab <kinglykrab@gmail.com>
2025-03-06 00:47:51 -0500

    Update and Rename #Coirnav_the_Avatar_of_Water.pl to #Coirnav_the_Avatar_of_Water.lua

---
+++ powater/#Coirnav_the_Avatar_of_Water.lua
@@ -0,0 +1,18 @@
+function event_signal(e)
+	if e.signal == 7 then
+		e.self:Shout("Violaters of this plane be banished from this domain!")
+	end
+end
+
+function event_death_complete(e)
+	eq.signal(216107, 5) -- #coirnav_controller
+end
+
+function event_killed_merit(e)
+	local coirnav_bucket = tonumber(e.other:GetAccountBucket("pop.flags.coirnav")) or 0
+	if coirnav_bucket == 0 then
+		e.other:SetAccountBucket("pop.flags.coirnav", "1")
+		e.other:SummonItem(29163) -- Item: Sphere of Coalesced Water
+		e.other:Message(MT.LightBlue, "You receive a character flag!")
+	end
+end
