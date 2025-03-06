242f9af2b25d9a0676256ac400dc95f8903d06dd
Kinglykrab <kinglykrab@gmail.com>
2025-03-06 01:02:39 -0500

    Update #Essence_of_Earth.lua

--- poearthb/#Essence_of_Earth.lua
+++ poearthb/#Essence_of_Earth.lua
@@ -11,8 +11,11 @@ end
 
 function event_say(e)
 	if e.message:findi("Hail") then
-		e.other:SummonItem(29146) -- Item: Mound of Living Stone
-		e.other:SetAccountBucket("pop.flags.rathe", "1")
-		e.other:Message(MT.LightBlue, "You receive a character flag!")
+		local rathe_bucket = tonumber(e.other:GetAccountBucket("pop.flags.rathe")) or 0
+		if rathe_bucket == 0 then
+			e.other:SummonItem(29146) -- Item: Mound of Living Stone
+			e.other:SetAccountBucket("pop.flags.rathe", "1")
+			e.other:Message(MT.LightBlue, "You receive a character flag!")
+		end
 	end
 end
