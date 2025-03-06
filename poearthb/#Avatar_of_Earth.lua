f059a881cebf8f938523d7cc47b701cae00a14a6
Kinglykrab <kinglykrab@gmail.com>
2025-03-06 01:02:04 -0500

    Update and Rename #Avatar_of_Earth.pl to #Avatar_of_Earth.lua

---
+++ poearthb/#Avatar_of_Earth.lua
@@ -0,0 +1,14 @@
+function event_death_complete(e)
+	eq.spawn2(222015, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading()); -- #Essence_of_Earth
+	eq.signal(222012);
+	eq.set_global("poeb_rathe", 1, 3, "M30");
+end
+
+function event_killed_merit(e)
+	local rathe_bucket = tonumber(e.other:GetAccountBucket("pop.flags.rathe")) or 0
+	if rathe_bucket == 0 then
+		e.other:SummonItem(29146) -- Item: Mound of Living Stone
+		e.other:SetAccountBucket("pop.flags.rathe", "1")
+		e.other:Message(MT.LightBlue, "You receive a character flag!")
+	end
+end
