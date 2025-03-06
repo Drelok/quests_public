9207720a797f045657ba96b2d0acf898374cc4cb
Kinglykrab <kinglykrab@gmail.com>
2025-03-06 00:54:23 -0500

    Update 215438.lua

--- poair/215438.lua
+++ poair/215438.lua
@@ -11,8 +11,11 @@ end
 
 function event_say(e)
 	if e.message:findi("Hail") then
-		e.other:SetAccountBucket("pop.flags.xegony", "1")
-		e.other:Message(MT.LightBlue, "You receive a character flag!")
-		e.other:SummonItem(29164) -- Item: Amorphous Cloud of Air
+		local xegony_bucket = tonumber(e.other:GetAccountBucket("pop.flags.xegony")) or 0
+		if xegony_bucket == 0 then
+			e.other:SetAccountBucket("pop.flags.xegony", "1")
+			e.other:SummonItem(29164) -- Item: Amorphous Cloud of Air
+			e.other:Message(MT.LightBlue, "You receive a character flag!")
+		end
 	end
 end
