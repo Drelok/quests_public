fcf10dcc38804e31a436687fbd2a330c603353ce
Kinglykrab <kinglykrab@gmail.com>
2025-03-06 00:22:12 -0500

    Update 214105.lua

--- solrotower/214105.lua
+++ solrotower/214105.lua
@@ -11,7 +11,10 @@ end
 
 function event_say(e)
 	if e.message:findi("Hail") then
-		e.other:SetAccountBucket("pop.flags.xuzl", "1")
-		e.other:Message(MT.LightBlue, "You receive a character flag!")
+		local xuzl_bucket = tonumber(e.other:GetAccountBucket("pop.flags.xuzl")) or 0
+		if xuzl_bucket == 0 then
+			e.other:SetAccountBucket("pop.flags.xuzl", "1")
+			e.other:Message(MT.LightBlue, "You receive a character flag!")
+		end
 	end
 end
