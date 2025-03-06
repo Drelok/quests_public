e66c0c8baf569d1e2e4abe1f0142de48b44a0a72
Kinglykrab <kinglykrab@gmail.com>
2025-03-06 00:57:32 -0500

    Update #Essence_of_Water.lua

--- powater/#Essence_of_Water.lua
+++ powater/#Essence_of_Water.lua
@@ -13,8 +13,8 @@ function event_say(e)
 	if e.message:findi("Hail") then
 		local coirnav_bucket = tonumber(e.other:GetAccountBucket("pop.flags.coirnav")) or 0
 		if coirnav_bucket == 0 then
-			e.other:SetAccountBucket("pop.flags.coirnav", "1")
 			e.other:SummonItem(29163) -- Item: Sphere of Coalesced Water
+			e.other:SetAccountBucket("pop.flags.coirnav", "1")
 			e.other:Message(MT.LightBlue, "You receive a character flag!")
 		end
 	end
