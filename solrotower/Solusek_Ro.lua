9fc72eff428573fa13d177ad9762cda252620751
Kinglykrab <kinglykrab@gmail.com>
2025-03-06 00:43:38 -0500

    Update Solusek_Ro.lua

--- solrotower/Solusek_Ro.lua
+++ solrotower/Solusek_Ro.lua
@@ -1,27 +1,35 @@
-function event_death_complete(e)
-	eq.spawn2(218068,0,0, 0, -823, 243, 255);	--A_Planar_Projection
-	eq.get_entity_list():FindDoor(38):SetLockPick(0); --unlock fire chute
-	eq.get_entity_list():FindDoor(39):SetLockPick(0); --unlock fire chute
-end
-
 function event_combat(e)
-	if (e.joined == true) then
-		eq.set_timer("OOBcheck", 6 * 1000); -- 6 Sec OOB Check
+	if e.joined then
+		eq.set_timer("Out of Bounds", 6 * 1000) -- 6 Seconds
 	else
-		eq.stop_timer("OOBcheck");
+		eq.stop_timer("Out of Bounds")
 	end
 end
 
 
 function event_timer(e)
-	if(e.timer=="OOBcheck") then
-	eq.stop_timer("OOBcheck");
-		if (e.self:GetZ() < 200) then
-			e.self:Say("If you wish to challenge me, you must do it on my terms!");
-			e.self:GotoBind();
-			e.self:WipeHateList();
+	if e.timer == "Out of Bounds" then
+		eq.stop_timer("Out of Bounds")
+		if e.self:GetZ() < 200 then
+			e.self:Say("If you wish to challenge me, you must do it on my terms!")
+			e.self:GotoBind()
+			e.self:WipeHateList()
 		else
-			eq.set_timer("OOBcheck", 6 * 1000); -- 6 Sec OOB Check
+			eq.set_timer("Out of Bounds", 6 * 1000) -- 6 Seconds
 		end
+	end
+end
+
+function event_death_complete(e)
+	eq.spawn2(218068, 0, 0, 0, -823, 243, 255)	--A_Planar_Projection
+	eq.get_entity_list():FindDoor(38):SetLockPick(0) --unlock fire chute
+	eq.get_entity_list():FindDoor(39):SetLockPick(0) --unlock fire chute
+end
+
+function event_killed_merit(e)
+	local solusek_bucket = tonumber(e.other:GetAccountBucket("pop.flags.solusek")) or 0
+	if solusek_bucket == 0 then
+		e.other:SetAccountBucket("pop.flags.solusek", "1")
+		e.other:Message(MT.LightBlue, "You receive a character flag!")
 	end
 end
