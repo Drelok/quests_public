98521abf04de78c66216145c08c4d5d4bcff232b
RetributionEQ <info@retributioneq.com>
2025-03-10 23:13:40 -0400

    Remove Softblur

--- poair/High_Councilman_of_the_Queen.lua
+++ poair/High_Councilman_of_the_Queen.lua
@@ -1,10 +1,10 @@
 function event_combat(e)
 if (e.joined == true) then
 eq.set_timer('Hardblur', 180 * 1000);
-eq.set_timer('Softblur', 6 * 1000);
+-- eq.set_timer('Softblur', 6 * 1000);
 else
 eq.stop_timer('Hardblur');
-eq.stop_timer('Softblur');
+-- eq.stop_timer('Softblur');
 end
 end
 
@@ -12,9 +12,9 @@ end
 function event_timer(e)
 	if (e.timer == 'Hardblur') then
 		e.self:WipeHateList();
-	elseif (e.timer == 'Softblur') then
-		if (math.random(100)<=50) then
-			e.self:WipeHateList();
-		end
+	--elseif (e.timer == 'Softblur') then
+		--if (math.random(100)<=50) then
+			--e.self:WipeHateList();
+		-- end
 	end
 end
