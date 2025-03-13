b7034debf7f1ae06bcaa8d028e71f0be62b2ba31
Kinglykrab <kinglykrab@gmail.com>
2025-03-12 21:22:07 -0400

    Update A_Planar_Projection.lua

--- nightmareb/A_Planar_Projection.lua
+++ nightmareb/A_Planar_Projection.lua
@@ -11,8 +11,12 @@ end
 
 function event_say(e)
 	local terris_bucket = tonumber(e.other:GetAccountBucket("pop.flags.terris")) or 0
-	if e.message:findi("Hail") and terris_bucket == 0 then
-		e.other:SetAccountBucket("pop.flags.terris", "1")
-		e.other:Message(MT.LightBlue, "You receive a character flag!")
+	if e.message:findi("Hail") then
+		if terris_bucket == 0 then
+			e.other:SetAccountBucket("pop.flags.terris", "1")
+			e.other:Message(MT.LightBlue, "You receive a character flag!")
+		else
+			e.self:Say("It looks like we've already spoken.")
+		end
 	end
 end
