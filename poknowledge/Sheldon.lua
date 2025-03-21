24350c5035bebc2fbde5cd18963b9566174ba32f
mute <natanx@gmail.com>
2025-03-20 19:40:40 -0700

    Add Sheldon

---
+++ poknowledge/Sheldon.lua
@@ -0,0 +1,7 @@
+function event_say(e)
+    if(e.message:findi("hail") then
+        e.self:Say("Greetings, hero! Grand Librarian Maelin has tasked me to assist adventurers with a way to hastily reach him. Please let me know if you would like to be [" .. eq.say_link("translocated") .. "] up to him.")
+    elseif(e.message:findi("translocate") then
+        e.other:MovePC(Zone.poknowledge, 908.10, 19, 389.13, 129);
+    end
+end
