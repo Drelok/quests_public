5a444f865e4d31dd2dfbff942ace76b6ee5352b9
Catapultam Habeo <alex@catapult.am>
2025-03-21 15:49:53 -0500

    stuff

--- plugins/cata_general.pl
+++ plugins/cata_general.pl
@@ -245,3 +245,58 @@ sub swap_items {
     # Add the swapped item with augments
     $client->AddItem($item_data);
 }
+
+sub cycle_time_items {
+    my ($client, $item_id, $slot_id) = @_;
+    
+    # Define item cycle order
+    my %next_item = (
+        20017731 => 20017734,
+        20017734 => 20017735,
+        20017735 => 20017815,
+        20017815 => 20017816,
+        20017816 => 20017817,
+        20017817 => 20017818,
+        20017818 => 20017731,
+    );
+    
+    # If current item isn't in our cycle, bail out
+    return unless exists $next_item{$item_id};
+    
+    # Get the next item in the cycle
+    my $dst_item = $next_item{$item_id};
+    
+    # Retrieve augment data for the current item
+    my @augments = (
+        $client->GetAugmentIDAt($slot_id, 0),
+        $client->GetAugmentIDAt($slot_id, 1),
+        $client->GetAugmentIDAt($slot_id, 2),
+        $client->GetAugmentIDAt($slot_id, 3),
+        $client->GetAugmentIDAt($slot_id, 4),
+        $client->GetAugmentIDAt($slot_id, 5),
+    );
+    
+    # Replace invalid augment values (-1) with 0
+    foreach my $augment (@augments) {
+        $augment = 0 if $augment == -1;
+    }
+    
+    # Remove the current item
+    $client->DeleteItemInInventory($slot_id, 0, 1);
+    
+    # Construct item data with augments and attunement
+    my $item_data = {
+        item_id       => $dst_item,
+        charges       => 1,
+        augment_one   => $augments[0],
+        augment_two   => $augments[1],
+        augment_three => $augments[2],
+        augment_four  => $augments[3],
+        augment_five  => $augments[4],
+        augment_six   => $augments[5],
+        attuned       => 1,
+    };
+    
+    # Add the new item with augments
+    $client->AddItem($item_data);
+}
