e93f683ddaae25db51de60407096026109cdcc61
mute <natanx@gmail.com>
2025-03-21 19:54:51 -0700

    pojustice fix

--- pojustice/201078.lua
+++ pojustice/201078.lua
@@ -38,8 +38,9 @@ function event_say(e)
 					local member_count = group:GroupCount()
 					for i = 0, member_count - 1 do
 						local member = group:GetMember(i)
-						if member:CalculateDistance(e.self:GetX(), e.self:GetY(), e.self:GetZ()) <= 150 then
-							member:MovePCInstance(201, eq.get_zone_instance_id(), 254, -1053, 73, 300)
+						if member ~= nil and member.valid and member:IsClient() and member:CalculateDistance(e.self:GetX(), e.self:GetY(), e.self:GetZ()) <= 150 then
+							local client = member:CastToClient();
+							client:MovePCInstance(201, eq.get_zone_instance_id(), 254, -1053, 73, 300)
 						end
 					end
 				else
