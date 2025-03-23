10ddcca27f32a0f9512cf6b5197ee3fa537557e9
Pippz <piattnd@gmail.com>
2025-03-23 15:11:53 -0600

    Remove timer from Coirnav for non-respawning dz

--- powater/#coirnav_controller.pl
+++ powater/#coirnav_controller.pl
@@ -133,7 +133,9 @@ sub KICK_ALL_PLAYERS {
 }
 
 sub SPAWN_WAVE1 {
-  quest::settimer(1,1200); # 20 Minute timer for the kickout event!!
+  if ($zone->GetInstanceVersion() ne quest::get_rule("Custom:StaticInstanceVersion")+0) {
+    quest::settimer(1,1200); # 20 Minute timer for the kickout event!! Don't do this in non-respawning instance
+  }
   quest::settimer(2,60); # 3 Minute timer for second wave spawn
   quest::settimer(3,120); # 5 Minute timer for third wave
   quest::settimer(4,600); # 10 Minute timer for the emote
