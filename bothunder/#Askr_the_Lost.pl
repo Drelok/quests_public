81ceb02124e1ef29bdc36b8fda5e6e405b162d8c
Pippz <piattnd@gmail.com>
2025-03-22 22:16:19 -0600

    Moving depop timer only to open world

--- bothunder/#Askr_the_Lost.pl
+++ bothunder/#Askr_the_Lost.pl
@@ -2,8 +2,10 @@ $instanceid = quest::GetInstanceID();
 
 
 sub EVENT_SPAWN {
-quest::say("All to me!");
-quest::settimer(1,1800);
+	quest::say("All to me!");
+	if ($zone->GetInstanceVersion() ne quest::get_rule("Custom:StaticInstanceVersion")+0){
+		quest::settimer(1,1800);
+	}
 }
 
 sub EVENT_TIMER  {
