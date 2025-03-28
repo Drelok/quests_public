548d966b495103818009ef579a2e4bd15140ca1a
David H Bross <dbrosswsu@gmail.com>
2025-03-28 00:15:33 -0500

    change 217 to pofire in spawn_condition

--- pofire/Guardian_of_Doomfire.lua
+++ pofire/Guardian_of_Doomfire.lua
@@ -1,6 +1,6 @@
 function event_death_complete(e)
                 eq.get_zone():SetVariable("Fennin", "1")
-                eq.spawn_condition(217, eq.get_zone_instance_id(), 3, 1)
+                eq.spawn_condition("pofire", eq.get_zone_instance_id(), 3, 1)
                 eq.spawn2(211057, 0, 0, -72.8,-2330.4,-117,384)
                 eq.spawn2(217076, 0,0,   -1499,    -935, -182.8, 252) -- NPC: #Fennin_Ro_the_Tyrant_of_Fire
                 eq.spawn2(217087, 0, 0, -855.2, -1652.9, -211.5, 123.2) -- NPC: a_doomfire_ragefiend
