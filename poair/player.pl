d7a6137d6e8bcb0d117ac47c78542e0f46d7c7df
Catapultam Habeo <alex@catapult.am>
2025-04-09 00:38:22 -0500

    keyring xegony door

--- poair/player.pl
+++ poair/player.pl
@@ -3,7 +3,8 @@ sub EVENT_CLICKDOOR {
     if($entity_list->FindDoor(1)->GetLockPick() != 0) {
       if(plugin::check_hasitem($client, 28638) || $client->KeyRingCheck(28638)) {
         $entity_list->FindDoor(1)->SetLockPick(0); #unlock the rainbow for 5 minutes
-		$entity_list->FindDoor(1)->SetKeyItem(0); #remove the key item so no one sees the message that door is locked.
+		    $entity_list->FindDoor(1)->SetKeyItem(0); #remove the key item so no one sees the message that door is locked.
+        $client->KeyRingAdd(28638);
         quest::settimer("XegDoorOpen",300);
       }
     }
@@ -13,7 +14,7 @@ sub EVENT_CLICKDOOR {
 sub EVENT_TIMER {
   if($timer eq "XegDoorOpen") {
     quest::stoptimer("XegDoorOpen");
-	$entity_list->FindDoor(1)->SetLockPick(-1); #lock the rainbow again
-	$entity_list->FindDoor(1)->SetKeyItem(28638); #put the key back on.
+	  $entity_list->FindDoor(1)->SetLockPick(-1); #lock the rainbow again
+	  $entity_list->FindDoor(1)->SetKeyItem(28638); #put the key back on.
   }
 }
