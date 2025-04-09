e40308d9916bdb0582bc3681c5b84598cc87bc72
Catapultam Habeo <alex@catapult.am>
2025-04-09 00:36:24 -0500

    keyring the symbol of torden

--- bothunder/player.lua
+++ bothunder/player.lua
@@ -37,10 +37,19 @@ function event_click_door(e)
 	if (door_id == 51) then
 		-- check if the person clicking has the Symbol of Torden item id:9433 or has GM status of 80+ with the GM flag on.
 		-- the point of checking both status and GM flag is so a dev with status > 80 can still pretend to be a non-GM.
-		-- by using the status, we ensure someone cannot bypass the check by another GM using "#gm on" on a player.
-		if (e.self:Admin() >= 80 and e.self:GetGM()) then
-			PortIntoTower(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 100, -765, -1735, 1270, 0, false);
+		-- by using the status, we ensure someone cannot bypass the check by another GM using "#gm on" on a player.			
+
+		local key_found = false;
+		if (e.self:KeyRingCheck(9433)) then
+			key_found = true;
 		elseif (e.self:HasItem(9433)) then
+			key_found = true;
+			e.self:KeyRingAdd(9433);
+		elseif (e.self:Admin() >= 80 and e.self:GetGM()) then
+			key_found = true;
+		end
+		
+		if (key_found) then
 			PortIntoTower(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 100, -765, -1735, 1270, 0, false);
 		else
 			SendGargoyles(209024);
