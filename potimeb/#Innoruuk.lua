a64ec3c87f5f71f67a4516243bdee0816fad4b52
Fryguy <fryguy503@gmail.com>
2025-04-25 14:01:34 -0400

    [PoTimeB] Inny Add Spawn, instance aggro random client in zone (range set by var DISTANCE_CHECK) (#158)
    * [PoTimeB] Inny Add Spawn, instance aggro random client in zone (range set by var DISTANCE_CHECK)
    
    - Entity List is called once but since all spawns should happen near instantly it should not cause a bad pointer unless a client is killed between spawn calls which is unlikely with no delays.
    
    * Updated to add aggro linking

--- potimeb/#Innoruuk.lua
+++ potimeb/#Innoruuk.lua
@@ -2,76 +2,75 @@
 --Phase 5 God
 --potimeb
 
-local event_mobs = {223231,223232};
+local EVENT_MOBS     = {223231,223232}
+local DISTANCE_CHECK = 1000 * 1000
 
 function event_spawn(e)
-	eq.set_next_hp_event(75);
+	eq.set_next_hp_event(75)
 end
 
 function event_hp(e)
-	if (e.hp_event == 75) then
-		SpawnAdds(e,3,4);
-		eq.set_next_hp_event(20);
-	elseif (e.hp_event == 20) then
-		SpawnAdds(e,4,5);
+	if e.hp_event == 75 then
+		SpawnAdds(e,3,4)
+		eq.set_next_hp_event(20)
+	elseif e.hp_event == 20 then
+		SpawnAdds(e,4,5)
 	end
 end
 
 function event_combat(e)
 	if e.joined then
-		eq.stop_timer("reset");
+		eq.stop_timer("reset")
+		eq.set_timer("aggro_link", 3 * 1000)
 	else
-		eq.set_timer("reset",5 * 60 * 1000);
+		eq.set_timer("reset", 5 * 60 * 1000)
+		eq.stop_timer("aggro_link")
 	end
 end
 
 function event_timer(e)
+	eq.stop_timer(e.timer)
 	if e.timer == "reset" then
-		eq.stop_timer("reset");
-		eq.set_next_hp_event(75);
-		for k,v in pairs(event_mobs) do
-			eq.depop_all(v);
+		eq.set_next_hp_event(75)
+		for k,v in pairs(EVENT_MOBS) do
+			eq.depop_all(v)
+		end
+	elseif e.timer == "aggro_link" then
+		eq.set_timer("aggro_link", 3 * 1000)
+
+		local npc_list =  eq.get_entity_list():GetNPCList();
+		for npc in npc_list.entries do
+			if npc.valid and not npc:IsEngaged() and (npc:GetNPCTypeID() == EVENT_MOBS[1] or npc:GetNPCTypeID() == EVENT_MOBS[2]) then
+				npc:AddToHateList(e.self:GetHateRandom(),1)
+			end
 		end
 	end
 end
 
-function SpawnAdds(e,min_adds,max_adds)	--spawns 4 adds (2 sets of same type)	
-	local rand = math.random(min_adds,max_adds);
-	local id1 = eq.spawn2(223231,0,0,210,315,5,257);	--guardian of the dark prince
-	if ( id1.valid and e.self:GetTarget() and e.self:GetTarget().valid ) then
-		id1:AddToHateList(e.self:GetTarget(), 1);
+function SpawnAdds(e,min_adds,max_adds)	-- spawns 4 adds (2 sets of same type)	
+	local SPAWN_COUNT = math.random(min_adds,max_adds)
+	local ENTITY_LIST = eq.get_entity_list()
+
+	eq.spawn2(EVENT_MOBS[1],0,0,210,315,5,257):AddToHateList(ENTITY_LIST:GetRandomClient(e.self:GetX(), e.self:GetY(), e.self:GetZ(), DISTANCE_CHECK)) -- NPC: Guardian of the dark prince
+	eq.spawn2(EVENT_MOBS[2],0,0,300,215,5,387):AddToHateList(ENTITY_LIST:GetRandomClient(e.self:GetX(), e.self:GetY(), e.self:GetZ(), DISTANCE_CHECK))	-- NPC: Hatebringer of Innoruuk
+
+	if SPAWN_COUNT >= 3 then
+		eq.spawn2(EVENT_MOBS[1],0,0,280,330,15,324):AddToHateList(ENTITY_LIST:GetRandomClient(e.self:GetX(), e.self:GetY(), e.self:GetZ(), DISTANCE_CHECK))
 	end
-	
-	local id2 = eq.spawn2(223232,0,0,300,215,5,387);	--Hatebringer of Innoruuk
-	if ( id2.valid and e.self:GetTarget() and e.self:GetTarget().valid ) then
-		id2:AddToHateList(e.self:GetTarget(), 1);
+
+	if SPAWN_COUNT >= 4 then
+		eq.spawn2(EVENT_MOBS[2],0,0,325,280,15,324):AddToHateList(ENTITY_LIST:GetRandomClient(e.self:GetX(), e.self:GetY(), e.self:GetZ(), DISTANCE_CHECK))
 	end
-	
-	if (rand >= 3) then 
-		local id3 = eq.spawn2(223231,0,0,280,330,15,324)
-		if ( id3.valid and e.self:GetTarget() and e.self:GetTarget().valid ) then
-			id3:AddToHateList(e.self:GetTarget(), 1);
-		end
-	end
-	
-	if (rand >= 4) then
-		local id4 = eq.spawn2(223232,0,0,325,280,15,324)
-		if ( id4.valid and e.self:GetTarget() and e.self:GetTarget().valid ) then
-			id4:AddToHateList(e.self:GetTarget(), 1);
-		end
-	end
-	if (rand == 5) then 
-		local id5 = eq.spawn2(eq.ChooseRandom(223231,223232),0,0,255,255,5,324)
-		if ( id5.valid and e.self:GetTarget() and e.self:GetTarget().valid ) then
-			id5:AddToHateList(e.self:GetTarget(), 1);
-		end
+
+	if SPAWN_COUNT == 5 then
+		eq.spawn2(eq.ChooseRandom(EVENT_MOBS[1],EVENT_MOBS[2]),0,0,255,255,5,324):AddToHateList(ENTITY_LIST:GetRandomClient(e.self:GetX(), e.self:GetY(), e.self:GetZ(), DISTANCE_CHECK))
 	end
 end
 
 function event_death_complete(e)
 	-- send a signal to the zone_status that I died
-	eq.signal(223097,223167); -- Add Lockout
-	eq.signal(223097,6);
+	eq.signal(223097,223167) -- Add Lockout
+	eq.signal(223097,6)
 
 	local killer = eq.get_entity_list():GetClientByID(e.killer_id)
 
