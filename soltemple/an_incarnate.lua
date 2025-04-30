223ab17e8dcdfca22675a63da6dd292ed36a255b
I bring dishonor to mother and father stones <201484118+Doljonijiarnimorinar@users.noreply.github.com>
2025-04-30 16:07:32 -0400

    FIX EXPLOIT? (#165)
    every button I press on this website makes things worse
    
    Co-authored-by: djgouac <86606378+djgouac@users.noreply.github.com>

--- soltemple/an_incarnate.lua
+++ soltemple/an_incarnate.lua
@@ -1,11 +1,8 @@
--- 1 is no pop , 2 is 1 repop, 3 is 2 repop
+-- 1-9 is nothing, 10 is repop. Adjusted from original version due to an exploit that could cause these mobs to replicate endlessly as the original roll was 1d3, with one being nothing, two being respawn, three being spawn two copies.
 
 function event_death_complete(e)
-  local which = math.random(3);
-  if  (which == 2) then
-    eq.spawn2(80043, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()); -- NPC: an_incarnate
-  elseif (which == 3) then
-    eq.spawn2(80043, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()); -- NPC: an_incarnate
+  local which = math.random(10);
+  if  (which == 10) then
     eq.spawn2(80043, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()); -- NPC: an_incarnate
   end
 end
