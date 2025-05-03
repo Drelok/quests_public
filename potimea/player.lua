78032586c8aefa22e7c18576b1c03343a7395617
Pippz <piattnd@gmail.com>
2025-05-03 15:36:42 -0600

    Implementing potimeb skip feature

--- potimea/player.lua
+++ potimea/player.lua
@@ -2,7 +2,7 @@
 
 local expedition_info = {
     expedition = { name = "The Prison of the Forsaken", min_players = 1, max_players = 72 },
-    instance   = { zone = "potimeb", version = 255, duration = 604800 }, -- 7 Days
+    instance   = { zone = "potimeb", version = 255, duration = 50400 }, -- 14 Hours
     safereturn = { zone = "potimea", x = -37, y = -110, z = 6.08, h = 0.0 },
     zonein     = { x = -9, y = -2466, z = -79, h = 0 }
 }
@@ -23,40 +23,42 @@ function event_click_door(e)
 			-- Expedition and instance are valid - Start the port query
 			e.self:Message(MT.NPCQuestSay, "The portal flashes briefly, then glows steadily.")
 
-			local expedition_identifier = string.format("potime-%d-phase", expedition:GetID())
-			local phase_bucket = tonumber(eq.get_data(expedition_identifier)) or 0
+			--local expedition_identifier = string.format("potime-%d-phase", expedition:GetID())
+			--local phase_bucket = tonumber(eq.get_data(expedition_identifier)) or 0
 
 			--handles porting player to specified area if raid is currently in Phase 3 or further
-			if phase_bucket == 2 then
-				e.self:MovePCInstance(223, instance_id, 585, 1110, 496, 127)
-				return
-			elseif phase_bucket == 3 then
-				e.self:MovePCInstance(223, instance_id, -395, 0, 350, 127)
-				return
-			elseif phase_bucket == 4 then
-				e.self:MovePCInstance(223, instance_id, -410, 0, 5, 127)
-				return
-			elseif phase_bucket == 5 then
-				e.self:MovePCInstance(223, instance_id, 330, 0, 5, 127)
-				return
-			end
+			--if phase_bucket == 2 then
+			--	e.self:MovePCInstance(223, instance_id, 585, 1110, 496, 127)
+			--	return
+			--elseif phase_bucket == 3 then
+			--	e.self:MovePCInstance(223, instance_id, -395, 0, 350, 127)
+			--	return
+			--elseif phase_bucket == 4 then
+			--	e.self:MovePCInstance(223, instance_id, -410, 0, 5, 127)
+			--	return
+			--elseif phase_bucket == 5 then
+			--	e.self:MovePCInstance(223, instance_id, 330, 0, 5, 127)
+			--	return
+			--end
 
 			-- Phase 1/2
-			if door_id == 8 then
+			if door_id > 7 and door_id < 13 then
+				-- Move to potimeb graveyard
+				e.self:MovePCInstance(223, instance_id, 851, -141, 396.06, 0)
 				-- GetDoorID =  8 : Air Trial
-				e.self:MovePCInstance(223, instance_id, -36, 1352, 496, 124)
-			elseif door_id == 9 then
+				--e.self:MovePCInstance(223, instance_id, -36, 1352, 496, 124)
+			--elseif door_id == 9 then
 				-- GetDoorID =  9 : Water Trial
-				e.self:MovePCInstance(223, instance_id, -51, 857, 496, 124)
-			elseif door_id == 10 then
+				--e.self:MovePCInstance(223, instance_id, -51, 857, 496, 124)
+			--elseif door_id == 10 then
 				-- GetDoorID = 10 : Earth Trial
-				e.self:MovePCInstance(223, instance_id, -35, 1636, 496, 124)
-			elseif door_id == 11 then
+				--e.self:MovePCInstance(223, instance_id, -35, 1636, 496, 124)
+			--elseif door_id == 11 then
 				-- GetDoorID = 11 : Fire Trial
-				e.self:MovePCInstance(223, instance_id, -55, 569, 496, 124)
-			elseif door_id == 12 then
+				--e.self:MovePCInstance(223, instance_id, -55, 569, 496, 124)
+			--elseif door_id == 12 then
 				-- GetDoorID = 12 : Undead Trial
-				e.self:MovePCInstance(223, instance_id, -27, 1103, 496, 124)
+				--e.self:MovePCInstance(223, instance_id, -27, 1103, 496, 124)
 			end
 		else -- If not in an expedition, create one
 			if not e.self:HasExpeditionLockout("The Prison of the Forsaken", "Plane of Time") then
@@ -67,11 +69,11 @@ function event_click_door(e)
 						e.self:Message(MT.Red, "Instance failed to be created, yell at a GM")
 						return
 					end
-
-					expedition:AddReplayLockout(14 * 60 * 60) -- 14 Hours
+					expedition:AddReplayLockout(60)
+					--expedition:AddReplayLockout(14 * 60 * 60)
 
 					local expedition_identifier = string.format("potime-%d-phase", expedition:GetID())
-					eq.set_data(expedition_identifier, "0", "14H")
+					eq.set_data(expedition_identifier, "0", "1M")
 				end
 			else
 				e.self:Message(MT.Red, "You are currently locked out, try again once your lockout is over.")
